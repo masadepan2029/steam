739f8d00aa5429adea348ec08907e7852e21e4f8
masadepan2029 <93799779+masadepan2029@users.noreply.github.com>
2021-12-15 08:58:40 -0500

    Create radiocommands.c

---
+++ radiocommands.c
@@ -0,0 +1,120 @@
+Locations
+playerradio Radio.LocBombA "A bomb site"
+playerradio Radio.LocBombB "B bomb site"
+playerradio Radio.LocBack "Back"
+playerradio Radio.LocCatwalk "Catwalk"
+playerradio Radio.LocConnector "Connector"
+playerradio Radio.LocEnemySpawn "Enemy spawn"
+playerradio Radio.LocDoor "Door"
+playerradio Radio.LocDoubleDoors "Double door"
+playerradio Radio.LocHostageEscapeZone "Hostage escape zone"
+playerradio Radio.LocFront "Front"
+playerradio Radio.LocHostage "Hostage"
+playerradio Radio.LocHut "Hut"
+playerradio Radio.LocLong "Long"
+playerradio middle "Mid" (credit: hami)
+playerradio Radio.LocOverpass "Overpass"
+playerradio Radio.LocPalace "Palace"
+playerradio Radio.LocPlatform "platform"
+playerradio Radio.LocRamp "ramp"
+playerradio Radio.LocSnipersNest "snipersnest"
+playerradio Radio.LocStairs "Stairs"
+playerradio Radio.LocOurSpawn "Our spawn"
+
+Responces
+playerradio Agree "Yes"
+playerradio Disagree "No"
+playerradio Radio.Compliment "Good job"
+playerradio Radio.Sorry "I'm sorry"
+playerradio ScaredEmote "I'm scared"
+playerradio Radio.Thanks "Thanks"
+playerradio Radio.RoundLost "We lost the round"
+playerradio TeamLeadWon "We're winning"
+playerradio TeamLeadLost "We lost the lead"
+playerradio WonRound "We won"
+
+Combat Status
+playerradio Radio.ImAttacking "Attacking"
+playerradio CoveringFriend "Covering"
+playerradio PinnedDown "I'm pinned"
+playerradio InCombat "Taking fire"
+playerradio FriendlyFire "Taking friendly fire"
+playerradio LastManStanding "Last unit alive"
+
+Enemy Killed
+playerradio KilledMyEnemy "I killed them"
+playerradio Radio.MyHeadshot "I got a head shot"
+playerradio Radio.IKilledSniper "I killed a sniper"
+playerradio SniperKilled "We killed a sniper"
+playerradio Radio.SawHeadshot "I saw your headshot"
+playerradio niceshot "Nice Shot" (credit: Ace Clover)
+
+Enemies Seen
+playerradio Radio.EnemySpotted "I saw them"
+playerradio SniperWarning "Saw a sniper"
+playerradio SpottedBomber "Saw the bomb carrier"
+playerradio ThreeEnemiesLeft "Three left"
+playerradio TwoEnemiesLeft "Two left"
+playerradio OneEnemyLeft "One left"
+playerradio Clear "None left"
+
+Bomb Status
+playerradio GoingToGuardLooseBomb "Guarding the bomb"
+playerradio BombsiteClear "Bombsite clear"
+playerradio SpottedLooseBomb "Bomb has been dropped"
+playerradio Radio.PickedUpC4 "Bomb has been picked up"
+playerradio Radio.PlantingAtA "Planting at A"
+playerradio Radio.PlantingAtB "Planting at B"
+playerradio Radio.WePlanted "Planted"
+playerradio WhereIsTheBomb "Where is bomb?"
+playerradio TheyPickedUpTheBomb "They got bomb"
+playerradio GuardingLooseBomb "Guarding Loose Bomb"
+playerradio DefusingBomb "Defusing" (credit: Akezawa)
+playerradio SpottedBomber "I see the bomber"
+
+Request Move
+playerradio Radio.StickTogether "Stick together"
+playerradio Radio.FollowMe "Follow me"
+playerradio Radio.SpreadOut "Spread out"
+playerradio Radio.TeamFallBack "Fall back"
+playerradio Radio.GoGoGo "Go Go Go!"
+playerradio Radio.LocHostage "Get the hostage"
+playerradio EscortingHostages "Escorting Hostages"
+playerradio WaitingForHumanToDefuseBomb "Defuse Now"
+playerradio OnMyWay "I'm on my way"
+
+Request Prepare
+playerradio Radio.EcoRound "We should eco"
+playerradio Radio.SpendRound "We should spend"
+playerradio Radio.NeedLeader "Who will lead?"
+playerradio Radio.NeedPlan "What's the plan?"
+playerradio Radio.NeedDrop "Can I have a weapon?"
+
+Request Defend
+playerradio Radio.NeedQuiet "Be quiet!"
+playerradio Radio.CoverMe "Cover me"
+playerradio help "I need help"
+playerradio Radio.HoldPosition "Hold your position"
+playerradio guardinghostageescapezone "guarding their escape"
+
+Utility
+playerradio Radio.NeedDecoy "Need a Decoy"
+playerradio Radio.NeedSmoke "Need a Smoke"
+playerradio Radio.NeedFire "Need a Fire"
+playerradio Radio.NeedGrenade "Need a Grenade"
+playerradio Radio.NeedFlash "Need a Flashbang"
+playerradio Radio.Decoy "Decoy out"
+playerradio Radio.Smoke "Smoke out"
+playerradio Radio.Molotov Fire "Molotov out"
+playerradio Radio.FireInTheHole "Grenade out"
+playerradio Radio.Flashbang "Flashbang out"
+playerradio blinded "Flashed"
+
+Bonus
+playerradio DeathCry "*dies*"
+playerradio AgreeWithPlan "OK"
+playerradio WonRoundQuickly "Good Job"
+playerradio RequestReport "Report In"
+playerradio radio.cheer "*cheers*"
+playerradio HeardNoise "I heard something"
+playerradio killedFriend "Teamkill"
